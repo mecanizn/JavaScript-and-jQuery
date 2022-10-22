26edeb4cda2b1d0449846e040fa2b0affda547ab
Leandro Santos <mecanizn@gmail.com>
2022-10-22 02:36:02 -0300

    firt commit

---
+++ script.js
@@ -0,0 +1,56 @@
+//Comentario de uma linha
+/*Comentarios de multiplas linhas
+alert('Arquivo.js');
+
+//Variaveis
+var msg = 'variavel';
+alert(msg);
+
+//Operadores
+var a = prompt('variavel a');
+var b = prompt('Variavel b');
+var soma = a+b;
+var sub = a-b;
+var div = a/b;
+var mult = a*b; 
+
+alert(soma);
+alert(sub);
+alert(div);
+alert(mult);
+
+
+// Estruturas condicionais
+var c = prompt('variavel c');
+
+if(c % 2 == 0){
+    alert('O Numero é Par');
+}else{
+    alert('O Numero é Impar');
+}
+
+
+
+//Laços
+//While--------------------------->>>>Enquanto
+var i = 0;
+while(i < 3){
+    alert(i);
+    i += 1
+}
+
+//For----------------------------->>>>Para
+for(var j = 0; j < 3; j++){
+    alert(j)
+}
+*/
+
+//Tipos de dados
+var inteiro = 5;
+var decimal = 5.5;
+var string = 'hello moto';
+var lista = ['laranja', inteiro, decimal, 'maçã', 'banana', string];
+
+for (i in lista){
+    alert(lista[i])
+}
